860ff2056996177afa1d9fa3eebd8231888c946f
DanielDeSantos <danieldesantosgarres@gmail.com>
2024-11-12 17:05:59 +0100

    Add files via upload

---
+++ Actividad7 - Funciones/borrarProgramas.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [string]$programa
+)
+
+function Desinstalar-Programa {
+    param (
+        [string]$nombrePrograma
+    )
+
+    $programaADesinstalar = Get-Package -Name "$nombrePrograma"
+
+    if ($programaADesinstalar) {
+        
+            Write-Host "Desinstalando '$nombrePrograma'..."
+            Get-Package -Name "$nombrePrograma" | Uninstall-Package
+            Write-Host "El programa '$nombrePrograma' ha sido desinstalado correctamente."
+    } else {
+        Write-Host "No se encontró ningún programa llamado '$nombrePrograma'."
+    }
+
+}
+
+if ($programa) {
+    Desinstalar-Programa -nombrePrograma $programa
+} else {
+    Write-Host "Por favor, proporciona el nombre de un programa para desinstalar."
+}
+
