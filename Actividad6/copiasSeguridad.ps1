a1346e1f54ac44e1a3a0455c9cd0e3f117cc921f
DanielDeSantos <danieldesantosgarres@gmail.com>
2024-11-12 16:56:07 +0100

    Add files via upload

---
+++ Actividad6/copiasSeguridad.ps1
@@ -0,0 +1,37 @@
+﻿param (
+    [string]$rutaDestino  # Coge la ruta del directorio de destino que ha sido pasada como parámetro
+)
+
+
+# Pregunta al usuario por la ruta de origen
+$rutaOrigen = Read-Host "Introduce la ruta del directorio que deseas respaldar"
+
+# Comprueba si la ruta de origen existe
+if (-not (Test-Path -Path $rutaOrigen)) {
+    Write-Host "El directorio de origen '$rutaOrigen' no existe."
+    exit
+}
+
+Write-Host (Test-Path -Path $rutaDestino)
+
+# Comprueba si la ruta de destino existe
+if (-not (Test-Path -Path $rutaDestino)) {
+    Write-Host "El directorio de destino '$rutaDestino' no existe. Se creará."
+    # Si no existe, crea el directorio de destino
+    $directorioCreado = New-Item -ItemType Directory -Path $rutaDestino
+    if ($directorioCreado) {
+        Write-Host "Directorio de destino '$rutaDestino' creado correctamente."
+    } else {
+        Write-Host "Ha habido un problema al crear el directorio de destino."
+        exit
+    }
+}
+
+# Realiza la copia de seguridad
+Write-Host "Iniciando la copia de seguridad desde '$rutaOrigen' a '$rutaDestino'..."
+
+# Intenta copiar los archivos
+Copy-Item -Path "$rutaOrigen" -Destination $rutaDestino -Recurse -Force -ErrorAction SilentlyContinue # Añado este último parámetro para que no se muestren posibles errores causados por forzar la cópia del contenido del directorio.
+
+Write-Host "La copia de seguridad se ha completado correctamente."
+ 
---
+++ Actividad6/localizarFicheros.ps1
@@ -0,0 +1,24 @@
+﻿$ruta = ""
+while ([string]::IsNullOrWhiteSpace($ruta)) {  # Para obligar al usuario a introducir una ruta (y así prevenir errores), he creado un bucle que obligue a mientras detecte que no se ha introducido ningún valor.
+    $ruta = Read-Host "Introduce una ruta"
+}
+
+
+$nombreBuscado = ""
+while ([string]::IsNullOrWhiteSpace($nombreBuscado)) { # Para obligar al usuario a introducir un nombre (y así prevenir errores), he creado un bucle que obligue a mientras detecte que no se ha introducido ningún valor.
+    $nombreBuscado = Read-Host "Introduce el nombre del fichero. También puedes introcucir un patrón como *.txt"
+}
+
+
+if (Test-Path -Path $ruta) { # Comprueba si la ruta existe y, si no existe, da un mensaje diciendo que la ruta no existe y se acaba el código
+    $resultados = Get-ChildItem -Path $ruta -Filter $nombreBuscado  # Busca archivos o directorios que coincidan con el nombre o patrón indicado dentro de la ruta especificada
+    # Una vez se ha buscado el fichero o directorio en la ruta buscada, da una respuesta en función de los resultados obtenidos
+    if ($resultados) {
+        Write-Host "Se han encontrado los siguientes archivos o directorios:"
+        $resultados | ForEach-Object { Write-Host $_.FullName }
+    } else {
+        Write-Host "No se ha encontrado ningún archivo o directorio llamado '$nombreBuscado' en la ruta '$ruta'."
+    }
+} else {
+    Write-Host "La ruta especificada '$ruta' no existe."
+}
