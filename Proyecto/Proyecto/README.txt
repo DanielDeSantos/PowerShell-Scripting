43fe17227e978315c1c470132fc6a6b148a15b72
DanielDeSantos <danieldesantosgarres@gmail.com>
2024-11-26 17:06:31 +0100

    Add files via upload

---
+++ Proyecto/Proyecto/README.txt
@@ -0,0 +1,5 @@
+El script crearMaquina_Windows responde al ejercicio ""
+
+El script crearMaquina_Linux responde al ejercicio ""
+
+El script gestionarMaquinas responde al ejercicio ""
---
+++ Proyecto/Proyecto/crearMaquina.ps1
@@ -0,0 +1,23 @@
+﻿$VMName = Read-Host "NuevaVM"
+$VMPath = "$Env:USERPROFILE\VirtualBox VMs\$VMName"
+
+VBoxManage createvm --name $VMName --register *> $null
+
+VBoxManage modifyvm $VMName --memory 2048 --cpus 2 --os-type "Windows10_64" *> $null
+
+VBoxManage createmedium disk --filename "$VMPath\$VMName.vdi" --size 20480 --format VDI --variant Standard *> $null
+
+VBoxManage storagectl $VMName --name "USB" --add usb --controller usb *> $null
+VBoxManage storageattach $VMName --storagectl "USB" --port 0 --device 0 --type hdd --medium "$VMPath\$VMName.vdi" *> $null
+
+if (-Not (Test-Path -Path $VMPath)) {
+    New-Item -ItemType Directory -Path $VMPath
+}
+
+VBoxManage modifyvm $VMName --nic1 nat *> $null
+
+for ($i = 2; $i -le 8; $i++) {
+    VBoxManage modifyvm $VMName --nic$i intnet *> $null
+}
+
+Write-Host "La máquina virtual $VMName se ha creado con 8 tarjetas de red."
